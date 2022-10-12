e03dc9823cdce7b38dbc701f08de4532ef036899
Greg <gregperez90@gmail.com>
2022-10-11 23:38:22 -0400

    Generating random numbers but not words

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+//write a function getComputerChoice
+//when entered randomly returns Rock, Paper, or Scissors
+const word
+word[1] = 'Rock';
+word[2] = 'Paper';
+word[3] = 'Scissors';
+function getComputerChoice() {
+    return Math.floor(Math.random() * (a,b,c)); //Reading as numbers, need to read as words. Create an array?
+}
+
+console.log(getComputerChoice("Rock", "Paper", "Scissors"));
