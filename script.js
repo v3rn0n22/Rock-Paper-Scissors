b65409c15238c3f0bae81d517e1bede712d42274
Greg <gregperez90@gmail.com>
2022-10-13 23:06:53 -0400

    playerVScomputer working

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ function getComputerChoice() {
   return randomPick [Math.floor(Math.random() * randomPick.length)];
 }
 //Calling the function created
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Its a tie!'
+    }else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return 'Paper beats Rock, you LOSE!'
+    }else if (playerSelection == 'Scissor' && computerSelection == 'Rock') {
+        return 'Rock beats Scissors, you LOSE!'
+    }else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        return 'Scissor beats Paper, you LOSE!'
+    }else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        return 'Rock beats Scissors, you WIN!'
+    }else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        return 'Paper beats rock, you WIN!'
+    }else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        return 'Scissor beats Paper, you WIN!'
+    }
+
+}
+const playerSelection = 'Rock';
+const computerSelection = (getComputerChoice());
+console.log(playRound(playerSelection, computerSelection));
