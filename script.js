b6472e4881087a94db445b201ebff9528f1e715b
Greg <gregperez90@gmail.com>
2022-10-16 01:44:19 -0400

    Prompt works but need to figure out case sensativity

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+const playerSelection = prompt('Pick your Weapon'); //Declares the player choice
+const computerSelection = getComputerChoice(); //calls getComputerChoice function to declare the computers choice
+
+
 function getComputerChoice() { 
 //Created an array so variable = Rock,Paper,Scissors
   let randomPick = ['Rock', 'Paper', 'Scissors'];
 //Returns random element within the array. Math.floor/Math.random, randomizes the item picked in the array. .length assigns a number to the array ( in this case cycles through 3 options)
-  return randomPick [Math.floor(Math.random() * randomPick.length)];
+  return randomPick [Math.floor(Math.random() * 3)];
 }
-//Calling the function created
-//console.log(getComputerChoice());
+
+
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
 //If player and computer pick the same its a tie, if not the following determines who wins/loses
@@ -25,18 +30,16 @@ function playRound(playerSelection, computerSelection) {
     }
 
 }
-const playerSelection = 'Rock'; //Declares the player choice
-const computerSelection = getComputerChoice(); //calls getComputerChoice function to declare the computers choice
 
 console.log(playerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
 
-function game(playRound) {
+/*function game(playRound) {
     for(let i = 0; i< 5; i++){
-        
+        if (playerSelection )
         console.log(i);
     }
 }
-game(playRound);
-//let i = parseInt(prompt('Rock,Paper,Scissors?')) ;
+game(playRound);*/
+
